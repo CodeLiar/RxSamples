a284c3079844345bfd459a5deaa8419c389135c0
Geass <dmjliar@gmail.com>
2018-07-25 12:35:31 +0800

    vc

---
+++ RxSamples/RxSamples/BaseClass/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  RxSamples
+//
+//  Created by Geass on 7/20/18.
+//  Copyright © 2018 Geass. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+  }
+
+}
+
