d0a6fa6c7b4d16f2934021b791864d0fbf531902
Geass <dmjliar@gmail.com>
2018-07-25 12:35:22 +0800

    框架

--- RxSamples/RxSamples/BaseClass/NavigationController.swift
+++ RxSamples/RxSamples/BaseClass/NavigationController.swift
@@ -1,25 +1,24 @@
 //
-//  ViewController.swift
+//  NavigationController.swift
 //  RxSamples
 //
-//  Created by Geass on 7/20/18.
+//  Created by Geass on 7/25/18.
 //  Copyright © 2018 Geass. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class NavigationController: UINavigationController {
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // Do any additional setup after loading the view.
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
+  
 }
-
---
+++ RxSamples/RxSamples/BaseClass/TabBarController.swift
@@ -0,0 +1,24 @@
+//
+//  TabBarController.swift
+//  RxSamples
+//
+//  Created by Geass on 7/25/18.
+//  Copyright © 2018 Geass. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+}
